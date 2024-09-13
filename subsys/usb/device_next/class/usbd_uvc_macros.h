e75796352efcb083ad5b885d3f9d6fcd5a4bc8f8
Josuah Demangeon <me@josuah.net>
2024-09-13 12:23:18 +0200

    usb: device_next: uvc: use native devicetree booleans
    Instead of reconstruct booleans them from integers,
    use macros to loop over all booleans manually.
    
    Signed-off-by: Josuah Demangeon <me@josuah.net>

--- subsys/usb/device_next/class/usbd_uvc_macros.h
+++ subsys/usb/device_next/class/usbd_uvc_macros.h
@@ -17,19 +17,6 @@
 #ifndef ZEPHYR_INCLUDE_USBD_UVC_MACROS_H_
 #define ZEPHYR_INCLUDE_USBD_UVC_MACROS_H_
 
-/* Video and Still Image Payload Headers */
-#define UVC_BMHEADERINFO_FRAMEID		BIT(0)
-#define UVC_BMHEADERINFO_END_OF_FRAME		BIT(1)
-#define UVC_BMHEADERINFO_HAS_PRESENTATIONTIME	BIT(2)
-#define UVC_BMHEADERINFO_HAS_SOURCECLOCK	BIT(3)
-#define UVC_BMHEADERINFO_PAYLOAD_SPECIFIC_BIT	BIT(4)
-#define UVC_BMHEADERINFO_STILL_IMAGE		BIT(5)
-#define UVC_BMHEADERINFO_ERROR			BIT(6)
-#define UVC_BMHEADERINFO_END_OF_HEADER		BIT(7)
-
-/* Video Interface Class Code */
-#define UVC_CC_VIDEO				0x0E
-
 /* Video Interface Subclass Codes */
 #define SC_UNDEFINED				0x00
 #define SC_VIDEOCONTROL				0x01
@@ -116,8 +103,216 @@
 #define U64_LE(n) U(n, 0), U(n, 8), U(n, 16), U(n, 24), U(n, 32), U(n, 40), U(n, 48), U(n, 56)
 #define GUID(node) DT_FOREACH_PROP_ELEM_SEP(node, guid, DT_PROP_BY_IDX, (,))
 
+/* UVC controls for "zephyr,uvc-control-selector" */
+#define SU_INPUT_SELECT_CONTROL			0x01
+
+/* UVC controls for "zephyr,uvc-control-camera" */
+#define CT_SCANNING_MODE_CONTROL		0x01
+#define CT_SCANNING_MODE_BIT			0
+#define CT_AE_MODE_CONTROL			0x02
+#define CT_AE_MODE_BIT				1
+#define CT_AE_PRIORITY_CONTROL			0x03
+#define CT_AE_PRIORITY_BIT			2
+#define CT_EXPOSURE_TIME_ABSOLUTE_CONTROL	0x04
+#define CT_EXPOSURE_TIME_ABSOLUTE_BIT		3
+#define CT_EXPOSURE_TIME_RELATIVE_CONTROL	0x05
+#define CT_EXPOSURE_TIME_RELATIVE_BIT		4
+#define CT_FOCUS_ABSOLUTE_CONTROL		0x06
+#define CT_FOCUS_ABSOLUTE_BIT			5
+#define CT_FOCUS_RELATIVE_CONTROL		0x07
+#define CT_FOCUS_RELATIVE_BIT			6
+#define CT_FOCUS_AUTO_CONTROL			0x08
+#define CT_FOCUS_AUTO_BIT			17
+#define CT_IRIS_ABSOLUTE_CONTROL		0x09
+#define CT_IRIS_ABSOLUTE_BIT			7
+#define CT_IRIS_RELATIVE_CONTROL		0x0A
+#define CT_IRIS_RELATIVE_BIT			8
+#define CT_ZOOM_ABSOLUTE_CONTROL		0x0B
+#define CT_ZOOM_ABSOLUTE_BIT			9
+#define CT_ZOOM_RELATIVE_CONTROL		0x0C
+#define CT_ZOOM_RELATIVE_BIT			10
+#define CT_PANTILT_ABSOLUTE_CONTROL		0x0D
+#define CT_PANTILT_ABSOLUTE_BIT			11
+#define CT_PANTILT_RELATIVE_CONTROL		0x0E
+#define CT_PANTILT_RELATIVE_BIT			12
+#define CT_ROLL_ABSOLUTE_CONTROL		0x0F
+#define CT_ROLL_ABSOLUTE_BIT			13
+#define CT_ROLL_RELATIVE_CONTROL		0x10
+#define CT_ROLL_RELATIVE_BIT			14
+#define CT_PRIVACY_CONTROL			0x11
+#define CT_PRIVACY_BIT				18
+#define CT_FOCUS_SIMPLE_CONTROL			0x12
+#define CT_FOCUS_SIMPLE_BIT			19
+#define CT_WINDOW_CONTROL			0x13
+#define CT_WINDOW_BIT				20
+#define CT_REGION_OF_INTEREST_CONTROL		0x14
+#define CT_REGION_OF_INTEREST_BIT		21
+
+/* UVC controls for "zephyr,uvc-control-processing" */
+#define PU_BACKLIGHT_COMPENSATION_CONTROL	0x01
+#define PU_BACKLIGHT_COMPENSATION_BIT		8
+#define PU_BRIGHTNESS_CONTROL			0x02
+#define PU_BRIGHTNESS_BIT			0
+#define PU_CONTRAST_CONTROL			0x03
+#define PU_CONTRAST_BIT				1
+#define PU_GAIN_CONTROL				0x04
+#define PU_GAIN_BIT				9
+#define PU_POWER_LINE_FREQUENCY_CONTROL		0x05
+#define PU_POWER_LINE_FREQUENCY_BIT		10
+#define PU_HUE_CONTROL				0x06
+#define PU_HUE_BIT				2
+#define PU_SATURATION_CONTROL			0x07
+#define PU_SATURATION_BIT			3
+#define PU_SHARPNESS_CONTROL			0x08
+#define PU_SHARPNESS_BIT			4
+#define PU_GAMMA_CONTROL			0x09
+#define PU_GAMMA_BIT				5
+#define PU_WHITE_BALANCE_TEMPERATURE_CONTROL	0x0A
+#define PU_WHITE_BALANCE_TEMPERATURE_BIT	6
+#define PU_WHITE_BALANCE_TEMPERATURE_AUTO_CONTROL 0x0B
+#define PU_WHITE_BALANCE_TEMPERATURE_AUTO_BIT	12
+#define PU_WHITE_BALANCE_COMPONENT_CONTROL	0x0C
+#define PU_WHITE_BALANCE_COMPONENT_BIT		7
+#define PU_WHITE_BALANCE_COMPONENT_AUTO_CONTROL	0x0D
+#define PU_WHITE_BALANCE_COMPONENT_AUTO_BIT	13
+#define PU_DIGITAL_MULTIPLIER_CONTROL		0x0E
+#define PU_DIGITAL_MULTIPLIER_BIT		14
+#define PU_DIGITAL_MULTIPLIER_LIMIT_CONTROL	0x0F
+#define PU_DIGITAL_MULTIPLIER_LIMIT_BIT		15
+#define PU_HUE_AUTO_CONTROL			0x10
+#define PU_HUE_AUTO_BIT				11
+#define PU_ANALOG_VIDEO_STANDARD_CONTROL	0x11
+#define PU_ANALOG_VIDEO_STANDARD_BIT		16
+#define PU_ANALOG_LOCK_STATUS_CONTROL		0x12
+#define PU_ANALOG_LOCK_STATUS_BIT		17
+#define PU_CONTRAST_AUTO_CONTROL		0x13
+#define PU_CONTRAST_AUTO_BIT			18
+
+/* UVC controls for "zephyr,uvc-control-encoding" */
+#define EU_SELECT_LAYER_CONTROL			0x01
+#define EU_SELECT_LAYER_BIT			0
+#define EU_PROFILE_TOOLSET_CONTROL		0x02
+#define EU_PROFILE_TOOLSET_BIT			1
+#define EU_VIDEO_RESOLUTION_CONTROL		0x03
+#define EU_VIDEO_RESOLUTION_BIT			2
+#define EU_MIN_FRAME_INTERVAL_CONTROL		0x04
+#define EU_MIN_FRAME_INTERVAL_BIT		3
+#define EU_SLICE_MODE_CONTROL			0x05
+#define EU_SLICE_MODE_BIT			4
+#define EU_RATE_CONTROL_MODE_CONTROL		0x06
+#define EU_RATE_CONTROL_MODE_BIT		5
+#define EU_AVERAGE_BITRATE_CONTROL		0x07
+#define EU_AVERAGE_BITRATE_BIT			6
+#define EU_CPB_SIZE_CONTROL			0x08
+#define EU_CPB_SIZE_BIT				7
+#define EU_PEAK_BIT_RATE_CONTROL		0x09
+#define EU_PEAK_BIT_RATE_BIT			8
+#define EU_QUANTIZATION_PARAMS_CONTROL		0x0A
+#define EU_QUANTIZATION_PARAMS_BIT		9
+#define EU_SYNC_REF_FRAME_CONTROL		0x0B
+#define EU_SYNC_REF_FRAME_BIT			10
+#define EU_LTR_BUFFER_CONTROL			0x0C
+#define EU_LTR_BUFFER_BIT			11
+#define EU_LTR_PICTURE_CONTROL			0x0D
+#define EU_LTR_PICTURE_BIT			12
+#define EU_LTR_VALIDATION_CONTROL		0x0E
+#define EU_LTR_VALIDATION_BIT			13
+#define EU_LEVEL_IDC_LIMIT_CONTROL		0x0F
+#define EU_LEVEL_IDC_LIMIT_BIT			14
+#define EU_SEI_PAYLOADTYPE_CONTROL		0x10
+#define EU_SEI_PAYLOADTYPE_BIT			15
+#define EU_QP_RANGE_CONTROL			0x11
+#define EU_QP_RANGE_BIT				16
+#define EU_PRIORITY_CONTROL			0x12
+#define EU_PRIORITY_BIT				17
+#define EU_START_OR_STOP_LAYER_CONTROL		0x13
+#define EU_START_OR_STOP_LAYER_BIT		18
+#define EU_ERROR_RESILIENCY_CONTROL		0x14
+#define EU_ERROR_RESILIENCY_BIT			19
+
+/* UVC controls for Video Streaming Interface */
+#define VS_PROBE_CONTROL			0x01
+#define VS_COMMIT_CONTROL			0x02
+#define VS_STILL_PROBE_CONTROL			0x03
+#define VS_STILL_COMMIT_CONTROL			0x04
+#define VS_STILL_IMAGE_TRIGGER_CONTROL		0x05
+#define VS_STREAM_ERROR_CODE_CONTROL		0x06
+#define VS_GENERATE_KEY_FRAME_CONTROL		0x07
+#define VS_UPDATE_FRAME_SEGMENT_CONTROL		0x08
+#define VS_SYNCH_DELAY_CONTROL			0x09
+
+#define ZEPHYR_UVC_CONTROL_CT(node, t)						\
+	DT_PROP(node, control_scanning_mode) << CT_SCANNING_MODE_##t |		\
+	DT_PROP(node, control_ae_mode) << CT_AE_MODE_##t |			\
+	DT_PROP(node, control_ae_priority) << CT_AE_PRIORITY_##t |		\
+	DT_PROP(node, control_exposure_time_absolute) << CT_EXPOSURE_TIME_ABSOLUTE_##t |\
+	DT_PROP(node, control_exposure_time_relative) << CT_EXPOSURE_TIME_RELATIVE_##t |\
+	DT_PROP(node, control_focus_absolute) << CT_FOCUS_ABSOLUTE_##t |	\
+	DT_PROP(node, control_focus_relative) << CT_FOCUS_RELATIVE_##t |	\
+	DT_PROP(node, control_focus_auto) << CT_FOCUS_AUTO_##t |		\
+	DT_PROP(node, control_iris_absolute) << CT_IRIS_ABSOLUTE_##t |		\
+	DT_PROP(node, control_iris_relative) << CT_IRIS_RELATIVE_##t |		\
+	DT_PROP(node, control_zoom_absolute) << CT_ZOOM_ABSOLUTE_##t |		\
+	DT_PROP(node, control_zoom_relative) << CT_ZOOM_RELATIVE_##t |		\
+	DT_PROP(node, control_pantilt_absolute) << CT_PANTILT_ABSOLUTE_##t |	\
+	DT_PROP(node, control_pantilt_relative) << CT_PANTILT_RELATIVE_##t |	\
+	DT_PROP(node, control_roll_absolute) << CT_ROLL_ABSOLUTE_##t |		\
+	DT_PROP(node, control_roll_relative) << CT_ROLL_RELATIVE_##t |		\
+	DT_PROP(node, control_privacy) << CT_PRIVACY_##t |			\
+	DT_PROP(node, control_focus_simple) << CT_FOCUS_SIMPLE_##t |		\
+	DT_PROP(node, control_window) << CT_WINDOW_##t |			\
+	DT_PROP(node, control_region_of_interest) << CT_REGION_OF_INTEREST_##t
+
+#define ZEPHYR_UVC_CONTROL_EU(node, t)						\
+	DT_PROP(node, control_select_layer) << EU_SELECT_LAYER_##t |		\
+	DT_PROP(node, control_profile_toolset) << EU_PROFILE_TOOLSET_##t |	\
+	DT_PROP(node, control_video_resolution) << EU_VIDEO_RESOLUTION_##t |	\
+	DT_PROP(node, control_min_frame_interval) << EU_MIN_FRAME_INTERVAL_##t |\
+	DT_PROP(node, control_slice_mode) << EU_SLICE_MODE_##t |		\
+	DT_PROP(node, control_rate_control_mode) << EU_RATE_CONTROL_MODE_##t |	\
+	DT_PROP(node, control_average_bitrate) << EU_AVERAGE_BITRATE_##t |	\
+	DT_PROP(node, control_cpb_size) << EU_CPB_SIZE_##t |			\
+	DT_PROP(node, control_peak_bit_rate) << EU_PEAK_BIT_RATE_##t |		\
+	DT_PROP(node, control_quantization_params) << EU_QUANTIZATION_PARAMS_##t |\
+	DT_PROP(node, control_sync_ref_frame) << EU_SYNC_REF_FRAME_##t |	\
+	DT_PROP(node, control_ltr_buffer) << EU_LTR_BUFFER_##t |		\
+	DT_PROP(node, control_ltr_picture) << EU_LTR_PICTURE_##t |		\
+	DT_PROP(node, control_ltr_validation) << EU_LTR_VALIDATION_##t |	\
+	DT_PROP(node, control_level_idc_limit) << EU_LEVEL_IDC_LIMIT_##t |	\
+	DT_PROP(node, control_sei_payloadtype) << EU_SEI_PAYLOADTYPE_##t |	\
+	DT_PROP(node, control_qp_range) << EU_QP_RANGE_##t |			\
+	DT_PROP(node, control_priority) << EU_PRIORITY_##t |			\
+	DT_PROP(node, control_start_or_stop_layer) << EU_START_OR_STOP_LAYER_##t |\
+	DT_PROP(node, control_error_resiliency) << EU_ERROR_RESILIENCY_##t
+
+#define ZEPHYR_UVC_CONTROL_PU(node, t)						\
+	DT_PROP(node, control_backlight_compensation) << PU_BACKLIGHT_COMPENSATION_##t |\
+	DT_PROP(node, control_brightness) << PU_BRIGHTNESS_##t |		\
+	DT_PROP(node, control_contrast) << PU_CONTRAST_##t |			\
+	DT_PROP(node, control_contrast) << PU_CONTRAST_AUTO_##t |		\
+	DT_PROP(node, control_gain) << PU_GAIN_##t |				\
+	DT_PROP(node, control_power_line_frequency) << PU_POWER_LINE_FREQUENCY_##t |\
+	DT_PROP(node, control_hue) << PU_HUE_##t |				\
+	DT_PROP(node, control_hue) << PU_HUE_AUTO_##t |				\
+	DT_PROP(node, control_saturation) << PU_SATURATION_##t |		\
+	DT_PROP(node, control_sharpness) << PU_SHARPNESS_##t |			\
+	DT_PROP(node, control_gamma) << PU_GAMMA_##t |				\
+	DT_PROP(node, control_white_balance_temperature) << PU_WHITE_BALANCE_TEMPERATURE_##t |\
+	DT_PROP(node, control_white_balance_temperature) << PU_WHITE_BALANCE_TEMPERATURE_AUTO_##t |\
+	DT_PROP(node, control_white_balance_component) << PU_WHITE_BALANCE_COMPONENT_##t |\
+	DT_PROP(node, control_white_balance_component) << PU_WHITE_BALANCE_COMPONENT_AUTO_##t |\
+	DT_PROP(node, control_digital_multiplier) << PU_DIGITAL_MULTIPLIER_##t |\
+	DT_PROP(node, control_digital_multiplier_limit) << PU_DIGITAL_MULTIPLIER_LIMIT_##t |\
+	DT_PROP(node, control_analog_video_standard) << PU_ANALOG_VIDEO_STANDARD_##t |\
+	DT_PROP(node, control_analog_lock_status) << PU_ANALOG_LOCK_STATUS_##t
+
+#define ZEPHYR_UVC_CONTROL_OT(node, t) 0
+#define ZEPHYR_UVC_CONTROL_IT(node, t) 0
+#define ZEPHYR_UVC_CONTROL_XU(node, t) 0
+
 /* Automatically assign Entity IDs based on entities order in devicetree */
-#define NODE_ID(entity) UTIL_INC(DT_NODE_CHILD_IDX(entity))
+#define NODE_ID(entity)								\
+	UTIL_INC(DT_NODE_CHILD_IDX(entity))
 
 /* Fetch the ID of a child node with the given compat */
 #define NODE_IF_COMPAT(node, compat)						\
@@ -128,17 +323,12 @@
 	NODE_ID(LOOKUP_NODE(node, compat))
 
 /* Connect the entities to their source(s) */
-#define VC_PROP_N_ID(entity, prop, n) NODE_ID(DT_PHANDLE_BY_IDX(entity, prop, n))
+#define VC_PROP_N_ID(entity, prop, n)						\
+	NODE_ID(DT_PHANDLE_BY_IDX(entity, prop, n))
 #define VC_SOURCE_ID(entity)							\
 	DT_FOREACH_PROP_ELEM_SEP(entity, source_entity, VC_PROP_N_ID, (,))
-#define VC_SOURCE_NUM(entity) DT_PROP_LEN(entity, source_entity)
-
-/* Convert a list of integers to an (uint64_t) bitmap */
-#define VC_CONTROL_BIT(entity, prop, n) BIT(DT_PROP_BY_IDX(entity, prop, n)) |
-#define VC_CONTROLS(entity)							\
-	(IF_ENABLED(DT_NODE_HAS_PROP(entity, control_ids), (			\
-		DT_FOREACH_PROP_ELEM(entity, control_ids, VC_CONTROL_BIT)	\
-	)) 0)
+#define VC_SOURCE_NUM(entity)							\
+	DT_PROP_LEN(entity, source_entity)
 
 /* Estimate the frame buffer size out of other fields */
 #define MAX_VIDEO_FRAME_BUFFER_SIZE(frame)					\
@@ -179,7 +369,7 @@
 	USB_DESC_CS_INTERFACE,				/* bDescriptorType */	\
 	VC_HEADER,					/* bDescriptorSubtype */\
 	U16_LE(0x0150),					/* bcdUVC */		\
-	U16_LE(12 + 1 + VC_TOTAL_LENGTH(node)),		/* wTotalLength */	\
+	U16_LE(VC_TOTAL_LENGTH(node)),			/* wTotalLength */	\
 	U32_LE(30000000),				/* dwClockFrequency */	\
 	0x01,						/* bInCollection */	\
 	0x01,						/* baInterfaceNr */
@@ -218,7 +408,7 @@
 	U16_LE(0),					/* wObjectiveFocalLengthMax */\
 	U16_LE(0),					/* wOcularFocalLength */\
 	0x03,						/* bControlSize */	\
-	U24_LE(VC_CONTROLS(entity)),			/* bmControls */
+	U24_LE(ZEPHYR_UVC_CONTROL_CT(entity, BIT)),	/* bmControls */
 
 /* 3.7.2.4 Selector Unit Descriptor */
 #define VC_SELECTOR_UNIT_DESCRIPTOR(entity)					\
@@ -239,7 +429,7 @@
 	VC_SOURCE_ID(entity),				/* bSourceID */		\
 	U16_LE(0),					/* wMaxMultiplier */	\
 	0x03,						/* bControlSize */	\
-	U24_LE(VC_CONTROLS(entity)),			/* bmControls */	\
+	U24_LE(ZEPHYR_UVC_CONTROL_PU(entity, BIT)),	/* bmControls */	\
 	0x00,						/* iProcessing */	\
 	0x00,						/* bmVideoStandards */
 
@@ -252,7 +442,7 @@
 	VC_SOURCE_ID(entity),				/* bSourceID */		\
 	0x00,						/* iEncoding */		\
 	0x03,						/* bControlSize */	\
-	U48_LE(VC_CONTROLS(entity)),			/* bmControls+Runtime */
+	U48_LE(ZEPHYR_UVC_CONTROL_PU(entity, BIT)),	/* bmControls+Runtime */
 
 /* 3.7.2.7 Extension Unit Descriptor */
 #define VC_EXTENSION_UNIT_DESCRIPTOR(entity)					\
@@ -265,30 +455,30 @@
 	VC_SOURCE_NUM(entity),				/* bNrInPins */		\
 	VC_SOURCE_ID(entity)				/* baSourceID */	\
 	0x08,						/* bControlSize */	\
-	U64_LE(VC_CONTROLS(entity)),			/* bmControls */	\
+	U64_LE(ZEPHYR_UVC_CONTROL_XU(entity, BIT)),	/* bmControls */	\
 	0x00,						/* iExtension */
 
 /* Video Control descriptor content of a node according to its type */
 #define VC_DESCRIPTOR(entity)							\
-	IF_ENABLED(DT_NODE_HAS_COMPAT(entity, zephyr_uvc_control_input), (	\
+	IF_ENABLED(DT_NODE_HAS_COMPAT(entity, zephyr_uvc_control_it), (		\
 		VC_INPUT_TERMINAL_DESCRIPTOR(entity)				\
 	))									\
-	IF_ENABLED(DT_NODE_HAS_COMPAT(entity, zephyr_uvc_control_camera), (	\
+	IF_ENABLED(DT_NODE_HAS_COMPAT(entity, zephyr_uvc_control_ct), (		\
 		VC_CAMERA_TERMINAL_DESCRIPTOR(entity)				\
 	))									\
-	IF_ENABLED(DT_NODE_HAS_COMPAT(entity, zephyr_uvc_control_output), (	\
+	IF_ENABLED(DT_NODE_HAS_COMPAT(entity, zephyr_uvc_control_ot), (		\
 		VC_OUTPUT_TERMINAL_DESCRIPTOR(entity)				\
 	))									\
-	IF_ENABLED(DT_NODE_HAS_COMPAT(entity, zephyr_uvc_control_selector), (	\
+	IF_ENABLED(DT_NODE_HAS_COMPAT(entity, zephyr_uvc_control_su), (		\
 		VC_SELECTOR_UNIT_DESCRIPTOR(entity)				\
 	))									\
-	IF_ENABLED(DT_NODE_HAS_COMPAT(entity, zephyr_uvc_control_processing), (	\
+	IF_ENABLED(DT_NODE_HAS_COMPAT(entity, zephyr_uvc_control_pu), (		\
 		VC_PROCESSING_UNIT_DESCRIPTOR(entity)				\
 	))									\
-	IF_ENABLED(DT_NODE_HAS_COMPAT(entity, zephyr_uvc_control_encoding), (	\
+	IF_ENABLED(DT_NODE_HAS_COMPAT(entity, zephyr_uvc_control_eu), (		\
 		VC_ENCODING_UNIT_DESCRIPTOR(entity)				\
 	))									\
-	IF_ENABLED(DT_NODE_HAS_COMPAT(entity, zephyr_uvc_control_extension), (	\
+	IF_ENABLED(DT_NODE_HAS_COMPAT(entity, zephyr_uvc_control_xu), (		\
 		VC_EXTENSION_UNIT_DESCRIPTOR(entity)				\
 	))
 
@@ -317,10 +507,10 @@
 	USB_DESC_CS_INTERFACE,				/* bDescriptorType */	\
 	VS_INPUT_HEADER,				/* bDescriptorSubtype */\
 	VS_NUM_FORMATS(node),				/* bNumFormats */	\
-	U16_LE(14 + VS_TOTAL_LENGTH(node)),		/* wTotalLength */	\
+	U16_LE(VS_TOTAL_LENGTH(node)),			/* wTotalLength */	\
 	0x81,						/* bEndpointAddress */	\
 	0x00,						/* bmInfo */		\
-	LOOKUP_ID(node, zephyr_uvc_control_output),	/* bTerminalLink */	\
+	LOOKUP_ID(node, zephyr_uvc_control_ot),		/* bTerminalLink */	\
 	0x00,						/* bStillCaptureMethod */\
 	0x00,						/* bTriggerSupport */	\
 	0x00,						/* bTriggerUsage */	\
