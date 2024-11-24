2e1879d2ede58812a1d835d2afc37daea25b5b42
Josuah Demangeon <me@josuah.net>
2024-11-24 17:29:51 +0100

    usb: device_next: usb3 support
    Introduce definitions to enable USB3 device controllers to be written.
    The USB3 support got added to the core implementaiton, and to the Loopback
    and CDC-ACM class only.
    
    Signed-off-by: Josuah Demangeon <me@josuah.net>

--- subsys/usb/device_next/usbd_device.h
+++ subsys/usb/device_next/usbd_device.h
@@ -27,6 +27,8 @@ static inline uint8_t usbd_get_num_configs(const struct usbd_context *const uds_
 		desc = uds_ctx->fs_desc;
 	} else if (USBD_SUPPORTS_HIGH_SPEED && speed == USBD_SPEED_HS) {
 		desc = uds_ctx->hs_desc;
+	} else if (USBD_SUPPORTS_SUPER_SPEED && speed == USBD_SPEED_SS) {
+		desc = uds_ctx->ss_desc;
 	} else {
 		return 0;
 	}
@@ -52,6 +54,8 @@ static inline void usbd_set_num_configs(struct usbd_context *const uds_ctx,
 		desc = uds_ctx->fs_desc;
 	} else if (USBD_SUPPORTS_HIGH_SPEED && speed == USBD_SPEED_HS) {
 		desc = uds_ctx->hs_desc;
+	} else if (USBD_SUPPORTS_SUPER_SPEED && speed == USBD_SPEED_SS) {
+		desc = uds_ctx->ss_desc;
 	} else {
 		return;
 	}
